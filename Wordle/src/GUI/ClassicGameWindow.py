0820dbda8de76e6aabf29e20f5391ab7fad66e29
JesterArc <125870978+JesterArc@users.noreply.github.com>
2025-06-02 23:26:55 +0200

    Refactorized the code
    The game screen is almost ready

--- Wordle/src/GUI/ClassicGameWindow.py
+++ Wordle/src/GUI/ClassicGameWindow.py
@@ -1,6 +1,5 @@
 import tkinter as tk
-from tkinter import ttk, font
-from src.GUI.GameRow import GameRow
+from src.GUI.GameRow import GameRows
 
 
 class ClassicGameWindow(tk.Frame):
@@ -11,64 +10,13 @@ class ClassicGameWindow(tk.Frame):
         self.gray = "#3A3A3C"
         self.black = "#121213"
 
-        for i in range(0, 13):
-            self.rowconfigure(i, weight=1)
-        for i in range(0, 14):
-            self.columnconfigure(i, weight=1)
+        self.columnconfigure(0, weight=2)
+        self.columnconfigure(1, weight=6)
+        self.columnconfigure(2, weight=2)
 
         self.configure(background=self.black)
         self.pack(fill=tk.BOTH, expand=True)
 
-        self.guessRow = GameRow(self, columnspan=5, row=0, column=4)
-        self.guessRow.setText(0, "W")
-        self.guessRow.setText(1, "O")
-        self.guessRow.setText(2, "R")
-        self.guessRow.setText(3, "K")
-        self.guessRow.setText(4, "S")
-        self.guessRow.getCell(2).configure(background="#B59F3B")
-        self.guessRow.getCell(3).configure(background="#538D4E")
+        self.GameRows = GameRows(self, rows=6)
+        self.GameRows.grid(column=1)
 
-        self.guessRow2 = GameRow(self, columnspan=5, row=1, column=4)
-        self.guessRow2.setText(0, "W")
-        self.guessRow2.setText(1, "O")
-        self.guessRow2.setText(2, "R")
-        self.guessRow2.setText(3, "K")
-        self.guessRow2.setText(4, "S")
-        self.guessRow2.getCell(2).configure(background="#538D4E")
-        self.guessRow2.getCell(3).configure(background="#B59F3B")
-
-        self.guessRow3 = GameRow(self, columnspan=5, row=2, column=4)
-        self.guessRow3.setText(0, "W")
-        self.guessRow3.setText(1, "O")
-        self.guessRow3.setText(2, "R")
-        self.guessRow3.setText(3, "K")
-        self.guessRow3.setText(4, "S")
-        self.guessRow3.getCell(2).configure(background="#B59F3B")
-        self.guessRow3.getCell(3).configure(background="#538D4E")
-
-        self.guessRow4 = GameRow(self, columnspan=5, row=3, column=4)
-        self.guessRow4.setText(0, "W")
-        self.guessRow4.setText(1, "O")
-        self.guessRow4.setText(2, "R")
-        self.guessRow4.setText(3, "K")
-        self.guessRow4.setText(4, "S")
-        self.guessRow4.getCell(2).configure(background="#B59F3B")
-        self.guessRow4.getCell(3).configure(background="#538D4E")
-
-        self.guessRow5 = GameRow(self, columnspan=5, row=4, column=4)
-        self.guessRow5.setText(0, "W")
-        self.guessRow5.setText(1, "O")
-        self.guessRow5.setText(2, "R")
-        self.guessRow5.setText(3, "K")
-        self.guessRow5.setText(4, "S")
-        self.guessRow5.getCell(2).configure(background="#B59F3B")
-        self.guessRow5.getCell(3).configure(background="#538D4E")
-
-        self.guessRow6 = GameRow(self, columnspan=5, row=5, column=4)
-        self.guessRow6.setText(0, "W")
-        self.guessRow6.setText(1, "O")
-        self.guessRow6.setText(2, "R")
-        self.guessRow6.setText(3, "K")
-        self.guessRow6.setText(4, "S")
-        self.guessRow6.getCell(2).configure(background="#B59F3B")
-        self.guessRow6.getCell(3).configure(background="#538D4E")
--- Wordle/src/GUI/GameRow.py
+++ Wordle/src/GUI/GameRow.py
@@ -3,17 +3,18 @@ from tkinter import font
 
 
 class GameRow(tk.Frame):
-    def __init__(self, master, row, column, columnspan=1):
+    def __init__(self, master, row, columnspan=1):
         super().__init__(master)
         self.gray = "#3A3A3C"
         self.master = master
-        f = font.Font(size=25, family="Comic Sans MS", weight="bold")
+        self.columns = columnspan
+        f = font.Font(size=30, family="Comic Sans MS", weight="bold")
         self.cells = dict()
+        self.isCurrentRow = False
         for r in range(0, columnspan):
             self.cells[f"text{r}"] = tk.StringVar()
-            self.cells[f"text{r}"].set(chr(ord("A") + r))
-            self.cells[f"cell{r}"] = tk.Label(self.master, width=2, textvariable=self.cells[f"text{r}"], font=f, background=self.gray, foreground='white', justify='center')
-            self.cells[f"cell{r}"].grid(row=row, column=column + r)
+            self.cells[f"cell{r}"] = tk.Label(self.master, width=2, textvariable=self.cells[f"text{r}"], font=f, background=self.gray, foreground='white', justify='center', padx=5, pady=5, highlightcolor="white", highlightthickness=1)
+            self.cells[f"cell{r}"].grid(row=row, column=r + 1)
 
     def getCell(self, count):
         return self.cells[f"cell{count}"]
@@ -24,3 +25,32 @@ class GameRow(tk.Frame):
     def getText(self, cell_count):
         return self.cells[f"text{cell_count}"]
 
+    def setCells(self, text):
+        for i in range(0, self.columns):
+            self.setText(i, text[i])
+
+
+class GameRows(tk.Frame):
+    def __init__(self, master, rows, columnspan=5):
+        super().__init__(master)
+        self.master = master
+
+        for i in range(0, 5):
+            self.columnconfigure(0, weight=1)
+        for i in range(0, 6):
+            self.rowconfigure(0, weight=1)
+
+        self.rows = dict()
+
+        for i in range(0, rows):
+            self.rows[f"row{i}"] = GameRow(self, columnspan=columnspan, row=i)
+
+        self.rows["row0"].isCurrentRow = True
+
+    def getRow(self, row):
+        return self.rows[f"row{row}"]
+
+    def getCurrentRow(self):
+        for key, value in self.rows.items():
+            if value.isCurrentRow:
+                return value
