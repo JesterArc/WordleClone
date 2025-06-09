642bb136246396245cab004e545dc350e03d4037
JesterArc <125870978+JesterArc@users.noreply.github.com>
2025-06-09 11:05:02 +0200

    extracted common method

--- Wordle/src/GUI/FrameTraveler.py
+++ Wordle/src/GUI/FrameTraveler.py
@@ -5,38 +5,31 @@ class FrameTraveler(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
 
-    def go_to_main_menu(self):
-        from src.GUI.MainMenu import MainMenu
+    def unload(self):
         for widget in self.winfo_children():
             widget.destroy()
         self.destroy()
+
+    def go_to_main_menu(self):
+        self.unload()
+        from src.GUI.MainMenu import MainMenu
         MainMenu(master=self.master)
 
     def go_to_game_window(self, rows=6, columns=5, hardmode=False, history=False, replay=list):
-        if replay is None:
-            replay = list()
-        for widget in self.winfo_children():
-            widget.destroy()
-        self.destroy()
+        self.unload()
         from src.GUI.GameWindow import GameWindow
         GameWindow(master=self.master, rows=rows, columns=columns, hardmode=hardmode, history=history, replay=replay)
 
     def go_to_customize_window(self):
-        for widget in self.winfo_children():
-            widget.destroy()
-        self.destroy()
+        self.unload()
         from src.GUI.CustomizeGame import CustomizeGame
         CustomizeGame(master=self.master)
 
     def go_to_run_history(self):
-        for widget in self.winfo_children():
-            widget.destroy()
-        self.destroy()
+        self.unload()
         from src.GUI.RunHistory import RunHistory
         RunHistory(master=self.master)
 
     def exit_game(self):
-        for widget in self.winfo_children():
-            widget.destroy()
-        self.destroy()
+        self.unload()
         self.master.destroy()
