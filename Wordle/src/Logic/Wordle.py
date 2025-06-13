2bdaa2aaed690a5b2a8ebad7cee6b0bccdff9b86
JesterArc <125870978+JesterArc@users.noreply.github.com>
2025-06-13 23:10:59 +0200

    Update Wordle.py

--- Wordle/src/Logic/Wordle.py
+++ Wordle/src/Logic/Wordle.py
@@ -16,19 +16,47 @@ class Validation(Enum):
 
 
 class Wordle:
-    def __init__(self, length=5, hardmode=False):
+    def __init__(self, length=5, hardMode=False):
+        """Provides the logic for the game
+
+        On creation, the class:\n
+        - Generates a word list of words of provided length
+        - Chooses the secret word for the player to guess
+        - Sets whether the game is in normal mode or hard mode
+
+        Args:
+            length (int): The length of the word
+            hardMode (bool): If the game is played in hard mode or normal mode
+        """
         self.wordList = getWords(length)
         self.length = length
         self.secretWord = self.chooseRandomWord()
         self.guessCounter = 0
-        self.hardMode = hardmode
+        self.hardMode = hardMode
         self.answerRequirement = ['' for _ in range(length)]
         self.requiredLetters = set()
 
     def chooseRandomWord(self):
         return r.choice(self.wordList)
 
-    def verifyWord(self, word):
+    def verifyWord(self, word: str) -> Validation:
+        """Checks if the provided word is valid:\n
+        - If the word has length equal to 5, we check if the word is in the list of words,
+        - If the word is too short, we return "TOO_SHORT"\n
+
+        In Hard mode, we add extra checks:\n
+        - If the word doesn't contain all letters in required letters, we return "NOT_ALLOWED"
+        - If the word doesn't match the answer requirement, we return "NOT_ALLOWED"
+
+        Otherwise, we return "ALLOWED"
+
+
+        Args:
+            word (str): a word provided by the player
+
+        Returns:
+            Validation: returns the validation of the word
+        """
         match self.hardMode:
             case False:
                 if len(word) != self.length:
@@ -51,7 +79,21 @@ class Wordle:
                         return Validation.NOT_ALLOWED
                 return Validation.ALLOWED
 
-    def rateAnswer(self, guess):
+    def rateAnswer(self, guess: str) -> list[tuple[Rating, str]]:
+        """Rates each letter in the answer:\n
+        - If it is in the right spot, it gets the "Correct" rating
+        - Else if the letter is in the word but not here, it gets the "Exists" rating
+        - Else it gets the "Wrong" rating
+
+        "Correct" letters get added to the answer requirements\n
+        "Correct" or "Exists" letters get added to required letters
+
+        Args:
+            guess (str): a word provided by the player
+
+        Returns:
+            list[tuple[Rating, str]] : a list of tuples with a rating of the letter and the letter itself
+        """
         # Mark the correct guesses
         comparator_guess, comparator_answer = check_for_right_letters(guess, self.secretWord)
         # Mark partially correct guesses
@@ -72,7 +114,16 @@ class Wordle:
         return rating
 
 
-def check_for_right_letters(guess, correct):
+def check_for_right_letters(guess, correct) -> tuple[str, str]:
+    """Marks where words contain the exact same letter with "-"
+
+    Args:
+        guess (str): a word provided by the player
+        correct (str): the word the player is trying to guess
+
+    Returns:
+        tuple[str, str] : a tuple of strings
+    """
     guess = list(guess)
     correct = list(correct)
     for i in range(len(guess)):
@@ -81,14 +132,14 @@ def check_for_right_letters(guess, correct):
     return "".join(guess), "".join(correct)
 
 
-def mark_repetitions(word) -> list:
+def mark_repetitions(word: str) -> list[str]:
     """Marks each repeating letter in word with a number of times it appeared before
 
     Args:
         word (str): a string of letters
 
     Returns:
-        list : list of letters
+        list[str] : list of letters
     """
     letters = list(word)
     count_repetitions = dict()
@@ -103,7 +154,18 @@ def mark_repetitions(word) -> list:
     return letters
 
 
-def getWords(length: int) -> list:
+def getWords(length: int) -> list[str]:
+    """Returns a list of words of the provided length
+
+    Args:
+        length (int): desired length of the words
+
+    Returns:
+        list : list of letters with x length
+
+    Raises:
+        ValueError: If length is less than 1
+    """
     if length < 1:
         raise ValueError("Length must be greater than or equal to 1.")
     elif length == 5:
